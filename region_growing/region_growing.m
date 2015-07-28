b4efc7f2989410bca3abc6ee6611eda81bbef748
Pierre Bellec <bellecp@iro.umontreal.ca>
2015-07-27 23:31:19 -0400

    Update region_growing.m

--- region_growing/region_growing.m
+++ region_growing/region_growing.m
@@ -12,6 +12,7 @@ niak_wget('target_test_niak_mnc1'); % download demo data set
 opt_g.min_nb_vol = 20;     % The minimum number of volumes for an fMRI dataset to be included. This option is useful when scrubbing is used, and the resulting time series may be too short.
 opt_g.min_xcorr_func = 0; % The minimum xcorr score for an fMRI dataset to be included. This metric is a tool for quality control which assess the quality of non-linear coregistration of functional images in stereotaxic space. Manual inspection of the values during QC is necessary to properly set this threshold.
 opt_g.min_xcorr_anat = 0; % The minimum xcorr score for an fMRI dataset to be included. This metric is a tool for quality control which assess the quality of non-linear coregistration of the anatomical image in stereotaxic space. Manual inspection of the values during QC is necessary to properly set this threshold.
+opt_g.type_files = 'glm_connectome'; % Specify to the grabber to prepare the files for the glm_connectome pipeline
 files_in.data = niak_grab_fmri_preprocess([path_data 'target_test_niak_mnc1-' gb_niak_target_test '/demoniak_preproc/'],opt_g).data; % Replace the folder by the path where the results of the fMRI preprocessing pipeline were stored. 
 
 %%%%%%%%%%%%%
